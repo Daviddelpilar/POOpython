595269c51ef04766521c6e1834794b58331f2674
Daviddelpilar <117951699+Daviddelpilar@users.noreply.github.com>
2022-11-29 13:54:44 +0100

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,28 @@
+class Producto():
+    def __init__(self,nombre,unidades,precio):
+        self.nombre=nombre
+        self.unidades=unidades
+        self.precio=precio
+
+    def Informacion(self):
+        print(f'el producto {self.nombre} cuesta {self.precio} y quedan {self.unidades}')
+
+
+class Servicio():
+    def __init__(self):
+        print('El servicio es basico')
+
+
+class Estandar(Servicio):
+    def consultarDetalle(self):
+        print('El servicio es estandar')
+
+
+class Premium(Servicio):
+    def consultarDetalle(self):
+        print('El servicio es premium')
+
+
+producto1=Producto('camisa', 10, 9.95)
+producto1.Informacion()
+
